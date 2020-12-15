57e26a8144e30655174760d168364ce34dcf8c4b
Zawatsuka <57181738+Zawatsuka@users.noreply.github.com>
2020-12-15 11:26:16 +0100

    ajout de l'ex 3

--- EXO3/index.php
+++ EXO3/index.php
@@ -1,11 +1,12 @@
+<?php setlocale (LC_TIME, 'fr_FR.utf8','fra'); ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EXO3</title>
 </head>
 <body>
-    
+    <?= (strftime("%A %d %B %Y")); ?>
 </body>
 </html>
